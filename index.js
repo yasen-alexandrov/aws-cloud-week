94f429dd1263030247210e9893eb791c67b29cef
yasen-alexandrov <aly2sf3@bosch.com>
2023-09-11 13:10:08 +0300

    update app.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res){
-    res.send("Hello World from Pulumi and AWS!!");
+    res.send("Hello World from Pulumi and AWS!!have a nice day!");
 }); 
 
 app.listen(80);
